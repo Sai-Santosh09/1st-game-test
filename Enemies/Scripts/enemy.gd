4ba9708f8884d3483ad15fb79a6c9e6265dce0f3
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-01-18 15:55:17 +0530

    Added basic enemy the slime for the 1st try

--- Enemies/Scripts/enemy.gd
+++ Enemies/Scripts/enemy.gd
@@ -1,39 +1,48 @@
-class_name Player extends CharacterBody2D
+class_name Enemy extends CharacterBody2D
 
-var cardinal_direction : Vector2 = Vector2.DOWN
+signal direction_changed( new_direction : Vector2 )
+signal enemy_damaged()
+
 const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
+
+@export var hp : int = 3
+
+var cardinal_direction : Vector2 = Vector2.ZERO
 var direction : Vector2 = Vector2.ZERO
+var player : Player
+var invulnerable : bool = false
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var state_machine: PlayerStateMachine = $StateMachine
-
-signal DirectionChanged( new_direction : Vector2 )
+@onready var state_machine: EnemyStateMachine = $EnemyStateMachine
 
 
+
+
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	PlayerManager.player = self
-	state_machine.Initialize(self)
+	state_machine.initialize( self )
+	player = PlayerManager.player
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process( _delta ):
-	direction = Vector2(
-		Input.get_axis("left" , "right"),
-		Input.get_axis("up" , "down")
-	).normalized()
+func _process(delta):
 	pass
 
 
 
 
-func _physics_process( _delta ):
+
+func _physics_process(delta):
 	move_and_slide()
 
 
-func SetDirection() -> bool:
+func SetDirection( _new_direction : Vector2 ) -> bool:
+	direction = _new_direction
 	if direction == Vector2.ZERO:
 		return false
 	
@@ -45,11 +54,10 @@ func SetDirection() -> bool:
 	
 	
 	cardinal_direction = new_dir
-	DirectionChanged.emit( new_dir )
+	direction_changed.emit( new_dir )
 	sprite. scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	
 	return true
-
 
 
 
