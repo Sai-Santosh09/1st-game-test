769d462bf31ccf1105ad964fd40e8be9298c9ce8
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-01-18 11:39:09 +0530

    1st try

---
+++ Player/Scripts/state.gd
@@ -0,0 +1,32 @@
+class_name State extends Node
+
+static var player : Player
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func Enter() -> void:
+	pass
+
+
+
+
+func Exit() -> void:
+	pass
+
+
+
+
+func Process( _delta : float ) -> State:
+	return null
+
+
+
+func Physics( _delta : float ) -> State:
+	return  null
+
+
+
+func Handleinput( _event : InputEvent ) -> State:
+	return null
